ce7d515a9b2d086c68844ff89759f394a784b523
Mainak <mainakgh1@gmail.com>
2026-08-21 09:17:08 +0530

    parking lot code begins

---
+++ class-28-parking-lot-LLD-code/Parking-Lot-LLD/src/HourlyBikePricingStrategy.java
@@ -0,0 +1,7 @@
+public class HourlyBikePricingStrategy implements IPricingStrategy {
+
+    @Override
+    public double calculateFee(Ticket ticket) {
+        return 0;
+    }
+}
---
+++ class-28-parking-lot-LLD-code/Parking-Lot-LLD/src/HourlyCarPricingStrategy.java
@@ -0,0 +1,7 @@
+public class HourlyCarPricingStrategy implements IPricingStrategy {
+
+    @Override
+    public double calculateFee(Ticket ticket) {
+        return 0;
+    }
+}
